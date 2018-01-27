14f35894642a8a7b38dbdfc9f513e3efa2428570
Jorge <jgeromero@users.noreply.github.com>
2018-01-26 22:41:41 -0500

    fix g-analytics
    fixing g-analytics

--- _site/js/master.js
+++ _site/js/master.js
@@ -24,18 +24,13 @@
   }
 })();
 
-var _gaq = [
-  ['_setAccount', 'ADDACCOUNT-HERE'],
-  ['_setCustomVar', 1, 'document.querySelector', document.querySelector + '', 3],
-  ['_trackPageview']
-];
-(function(d) {
-  var script = d.createElement('script'),
-      head   = d.getElementsByTagName('head')[0];
-  script.async = true;
-  script.src = 'http://www.google-analytics.com/ga.js';
-  head.insertBefore(script, head.firstChild);
-})(document);
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-113152148-1', 'auto');
+ga('send', 'pageview');
 
 document.documentElement.onclick = function(e) {
   e = e || window.event;
